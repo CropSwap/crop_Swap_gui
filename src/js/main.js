58f46fb7793604310d7bcd72d6c8131bd24e3fe2
ericquayson <ericquayson@github.com>
2015-11-02 16:51:08 -0500

    add postcrop page, add background images, add routing for new pages

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@ TODO: add UI as a dependency once installed
 
           signup.signupUser = function () {
             console.log('it works!');
-            $http.post("http://cropswap.herokuapp.com/users.json", signup.user)
+            $http.post(BASE_URL + "users.json" + "email", signup.user)
               .then(function() {
                 console.log('it works');
                 $location.path('/newact')
@@ -35,6 +35,43 @@ TODO: add UI as a dependency once installed
         },
         controllerAs: 'signup'
       })
+      .when('/newact', {
+        templateUrl: 'partials/newact.html',
+        controller: function($http, $location) {
+          var newact = this;
+
+          newact.user = {
+
+          };
+
+        newact.newactuser = function () {
+          console.log('its working');
+          $http.patch(BASE_URL + "users/user:id.json" + "name", newact.user)
+            .then(function() {
+              $location.path('/postcrop')
+            });
+        };
+      },
+      controllerAs: 'newact'
+      })
+
+      .when('/postcrop', {
+        templateUrl: 'partials/postcrop.html',
+        controller: function($http, $location) {
+          var postcrop = this;
+
+          postcrop.crop = {
+
+          };
+          postcrop.newcrop = function () {
+            console.log('your posting a crop!');
+            $http.post(BASE_URL + " " + " ", postcrop.newcrop)
+              .then(function() {
+                
+              })
+          }
+        };
+      });
   })
 
 })(); // end of cropswap module
