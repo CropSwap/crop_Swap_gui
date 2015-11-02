3f56db42fedb271190e93121ca74c0db2fc4a8ef
ericquayson <ericquayson@github.com>
2015-11-02 01:42:48 -0500

    add mobile and tablet view pages and imported into main.scss. add another todo

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ TODO: add var BASE_URL
 TODO: add $routeProvider service
 TODO: routing for each page
 TODO: controller for each page
+TODO: add UI as a dependency once installed
 */
 
+//IFFE
 ;(function () {
   angular.module('cropswap', ['ngRoute'])
 
-})(); // end of module
+})(); // end of cropswap module
