9ce989284c7b03b3c85dc2a6c0c442716b934fb1
ericquayson <ericquayson@github.com>
2015-11-02 12:51:25 -0500

    add new account partial in partial folder, scss and main.js. Add crop-swap-user.json dummbie file structure

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,22 @@ TODO: add UI as a dependency once installed
 
 //IFFE
 ;(function () {
-  angular.module('cropswap', ['ngRoute'])
+  angular.module('cropswap', ['ngRoute'], function($routeProvider) {
+    $routeProvider
+      .when('/newact', {
+        templateUrl: 'partials/newact.html',
+        controller: function($http, $location) {
+          var create = this;
+
+          create.newact = { };
+
+          newact.submit = function() {
+            console.log("it works");
+            $http.post()
+          }
+        },
+        controllerAs: 'newact'
+      })
+  })
 
 })(); // end of cropswap module
