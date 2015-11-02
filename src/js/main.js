3cfbeee5e7e792c9ea6e99eb5b0a76654b2b99c9
ericquayson <ericquayson@github.com>
2015-11-01 22:55:51 -0500

    add angular app name to main.js and index, ng-view to index and comments for following steps

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/* IFFE
+TODO: add var BASE_URL
+TODO: add $routeProvider service
+TODO: routing for each page
+TODO: controller for each page
+*/
+
+;(function () {
+  angular.module('cropswap', ['ngRoute'])
+
+})(); // end of module
