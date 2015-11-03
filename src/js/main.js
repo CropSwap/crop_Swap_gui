5572f486bd1e470c90b1a314d4e6521a252b5b91
ericquayson <ericquayson@github.com>
2015-11-03 00:07:15 -0500

    add more styles. add  and set the var to equal the param

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 /* IFFE
-TODO: add var BASE_URL
-TODO: add $routeProvider service
+TODO: add $routeParams to each page that will need different params. set it equal to a variable
 TODO: routing for each page
 TODO: controller for each page
 TODO: add UI as a dependency once installed
@@ -12,66 +11,76 @@ TODO: add UI as a dependency once installed
 
   angular.module('cropswap', ['ngRoute'], function($routeProvider) {
     $routeProvider
-      .when('/', {
-        redirectTo: '/signup'
-      })
-      .when('/signup', {
-        templateUrl: 'partials/signup.html',
-        controller: function($http, $location) {
-          var signup = this;
+    .when('/', {
+      redirectTo: '/signup'
+    })
+    .when('/signup', {
+      templateUrl: 'partials/signup.html',
+      controller: function($http, $location) {
 
-          signup.user = {
+        //var email = $routeParams.email;
+        var signup = this;
 
-          };
+        signup.user = {
 
-          signup.signupUser = function () {
-            console.log('it works!');
-            $http.post(BASE_URL + "users.json" + "email", signup.user)
-              .then(function() {
-                console.log('it works');
-                $location.path('/newact')
-              });
-          };
-        },
-        controllerAs: 'signup'
-      })
-      .when('/newact', {
-        templateUrl: 'partials/newact.html',
-        controller: function($http, $location) {
-          var newact = this;
+        };
 
-          newact.user = {
+        signup.signupUser = function () {
+          console.log('it works!');
+          $http.post(BASE_URL + "users.json" + "email", signup.user)
+          .then(function() {
+            console.log('it works');
+            $location.path('/newact')
+          });
+        };
+      },
+      controllerAs: 'signup'
+    })
+    .when('/newact', {
+      templateUrl: 'partials/newact.html',
+      controller: function($http, $location, $routeParams) {
+        var id = $routeParams.id;
+        var newact = this;
 
-          };
+        newact.user = {
+
+        };
 
         newact.newactuser = function () {
           console.log('its working');
-          $http.patch(BASE_URL + "users/user:id.json" + "name", newact.user)
-            .then(function() {
-              $location.path('/postcrop')
-            });
+          $http.patch(BASE_URL + "id.json" + "name", newact.user)
+          .then(function() {
+            $location.path('/postcrop')
+          });
         };
       },
       controllerAs: 'newact'
-      })
+    })
 
-      .when('/postcrop', {
-        templateUrl: 'partials/postcrop.html',
-        controller: function($http, $location) {
-          var postcrop = this;
+    .when('/postcrop', {
+      templateUrl: 'partials/postcrop.html',
+      controller: function($http, $location) {
 
-          postcrop.crop = {
+        // var id = $routeParams.id;
+        var postcrop = this;
 
-          };
-          postcrop.newcrop = function () {
-            console.log('your posting a crop!');
-            $http.post(BASE_URL + " " + " ", postcrop.newcrop)
-              .then(function() {
-                
-              })
-          }
+        postcrop.crop = {
+
         };
-      });
+        postcrop.newcrop = function () {
+          console.log('your posting a crop!');
+          $http.post(BASE_URL + " " + " ", postcrop.newcrop)
+          .then(function() {
+
+          });
+        };
+      },
+      controllerAs: 'postcrop'
+    })
+    .otherwise({
+      redirectTo: '/404.html',
+      templateUrl: 'partials/404.html'
+    });
   })
 
 })(); // end of cropswap module
